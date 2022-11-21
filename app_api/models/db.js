1852050607fd1508092732cdcf7040ddbfc3a02c
denizozv <denizozveren13@gmail.com>
2022-11-21 18:01:12 +0300

    odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose=require("mongoose");
 //var dbURI="mongodb://localhost/mekanbul";
-var dbURI="mongodb+srv://mekan32:mekan32@mekanbul.d9jbgop.mongodb.net/?retryWrites=true&w=majority";
+var dbURI="mongodb+srv://mekan32:mekan32@mekanbul.d9jbgop.mongodb.net/mekanbul?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI);
 function kapat(msg,callback){
